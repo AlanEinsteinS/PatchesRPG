fd9e24e04fe66eca2afc5e7f49b2d96f4a30ff0d
AlanEinsteinS <alaneinstein245@gmail.com>
2024-02-16 13:19:47 -0300

    patch 5.1

--- static/script.js
+++ static/script.js
@@ -70,4 +70,39 @@ function toggleDetalhesPatches6() {
   }
 }
 
+function exibirConteudo(patchId) {
+  var patchButtons = document.getElementsByClassName("patch-button");
+  for (var i = 0; i < patchButtons.length; i++) {
+    patchButtons[i].classList.remove("active");
+  }
+
+  var patchContents = document.getElementsByClassName("patch-content");
+  for (var i = 0; i < patchContents.length; i++) {
+    patchContents[i].classList.remove("active");
+  }
+
+  var button = document.getElementById(patchId + "-button");
+  button.classList.add("active");
+
+  var content = document.getElementById(patchId + "-content");
+  content.classList.add("active");
+
+  closeAllDetails(); // Chama a função para fechar todos os detalhes
+}
+
+function closeAllDetails() {
+  var patchDetails = document.getElementsByClassName("patch-details");
+  for (var i = 0; i < patchDetails.length; i++) {
+    patchDetails[i].style.display = "none";
+  }
+}
+
+// Adicione esta parte para fechar os detalhes quando um patch-button for clicado
+var patchButtons = document.getElementsByClassName("patch-button");
+for (var i = 0; i < patchButtons.length; i++) {
+  patchButtons[i].addEventListener("click", function() {
+    closeAllDetails();
+  });
+}
+
 
